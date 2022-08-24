3cce015141e65f6c991a222799c451d30d617e67
Dastan <super.dastno@yandex.ru>
2022-08-24 23:00:23 +0300

    Task3 Complete

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number % 2 ==1)
+{
+    Console.WriteLine("число нечетное");
+}
+else
+{
+    Console.WriteLine("число четное");
+}
